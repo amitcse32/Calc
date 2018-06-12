64226010b89e59e6c3eba51d898458abebfe9b2c
= <amitcse32@gmail.com>
2018-06-12 10:58:25 +0530

    Final code

--- app/src/main/java/com/cssoft/calc/MainActivity.java
+++ app/src/main/java/com/cssoft/calc/MainActivity.java
@@ -2,12 +2,74 @@ package com.cssoft.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String val1="";
+    String val2="";
+    String oprator="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void numberClick(View v) {
+
+
+
+        if(oprator.equals("")) {
+            TextView textView = findViewById(R.id.textViewOutPut);
+            String var = ((Button) v).getText().toString();
+            val1 += var;
+            textView.setText(val1);
+        }
+        else
+        {
+            TextView textView = findViewById(R.id.textViewOutPut);
+            String var = ((Button) v).getText().toString();
+            val2 += var;
+            textView.setText(val2);
+        }
+
+    }
+
+    public void onOpratorClick(View view)
+    {
+        String var=((Button)view).getText().toString();
+        if(var.equals("="))
+        {
+                if(oprator.equals("+"))
+                {
+                    int total=Integer.parseInt(val1)+Integer.parseInt(val2);
+                    TextView textView = findViewById(R.id.textViewOutPut);
+                    textView.setText(total+"");
+                }
+
+            if(oprator.equals("-"))
+            {
+                int total=Integer.parseInt(val1)-Integer.parseInt(val2);
+                TextView textView = findViewById(R.id.textViewOutPut);
+                textView.setText(total+"");
+            }
+            if(oprator.equals("X"))
+            {
+                int total=Integer.parseInt(val1)*Integer.parseInt(val2);
+                TextView textView = findViewById(R.id.textViewOutPut);
+                textView.setText(total+"");
+            }
+        }
+        else {
+            oprator = var;
+        }
+    }
+
 }
